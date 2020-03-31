f0c6515b4a28456f5568e6c6de30097c5820bf00
Lucas Alderete <lucasm.alderete@gmail.com>
2020-03-31 15:07:15 -0300

    Se agregan archivos. Hasta motor de vistas

---
+++ html/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use('/assets', express.static("assets"));
+
+app.set("view engine","ejs");
+
+app.get('/html',function(req,res){
+    res.sendFile('index.html',{
+        root : __dirname
+    });
+});
+
+app.get('/view',function(req,res){
+    res.render("index");
+});
+
+app.listen(4000);
